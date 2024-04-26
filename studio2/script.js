48b5a2d15e3c2a85d5c122a3cbac724e0e0655f9
Maggie <myuma@ucdavis.edu>
2024-04-25 23:48:21 -0700

    research

---
+++ studio2/script.js
@@ -0,0 +1,45 @@
+(function(){
+    let globalData;
+
+    let currentTime = "9AM";
+    const h1Element = document.querySelector('#coughDropsInfo');
+    const leftIcon = document.querySelector('#leftIcon');
+    const rightIcon = document.querySelector('#rightIcon');
+
+    async function getData() {
+        const coughDropData = await fetch('data/coughdrops.json');
+        const data = await coughDropData.json();
+        /* console.log(data); */
+        globalData = data;
+        updateH1();
+    }
+
+
+    function updateH1() {
+        /* console.log(currentTime); */
+        const currentCoughDrops = globalData[currentTime];
+        h1Element.innerHTML = `At ${currentTime} I ate ${currentCoughDrops} cough drops.`;
+    }
+
+    rightIcon.addEventListener('click',function(){
+        const times = Object.keys(globalData);
+        const currentIndex = times.indexOf(currentTime);
+        if (currentIndex > 0) {
+            currentTime = times[currentIndex - 1];
+            updateH1();
+        }
+    });
+
+    leftIcon.addEventListener('click',function(){
+        const times = Object.keys(globalData);
+        const currentIndex = times.indexOf(currentTime);
+        if (currentIndex < times.length - 1) {
+            currentTime = times[currentIndex + 1];
+            updateH1();
+        }
+    });
+    
+    
+    getData();
+    
+})();
