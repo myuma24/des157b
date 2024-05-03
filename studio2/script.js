28b53b1630fec1ea6a026366a3399438afd17327
myuma24 <147002196+myuma24@users.noreply.github.com>
2024-05-02 23:33:21 -0700

    check urself

--- studio2/script.js
+++ studio2/script.js
@@ -1,60 +1,56 @@
-(function(){
+(function() {
     let globalData;
-
-    const h1Element = document.querySelector('#coughDropsInfo');
-    const leftIcon = document.querySelector('#leftIcon');
-    const rightIcon = document.querySelector('#rightIcon');
-
-    async function getData() {
-        const coughDropData = await fetch('data/coughdrops.json');
-        const data = await coughDropData.json();
-        console.log(data);
+    async function getData(){
+        const dropsdata = await fetch('data/coughdrops.json');
+        const data = await dropsdata.json();
+        //console.log(data);
         globalData = data;
+        document.querySelector("#drops").innerHTML = outputHTML1(data);
+        document.querySelector("#picker").innerHTML = createSelectList(data);
     }
-
-    function outputData(data){
-
-        for (let key in data){
-            console.log(key, data[key]);
-            /* h1Element.innerHTML = `I ate ${data[key]} coughdrops at ${key}`; */
-        }
-        return;
+    
+    function outputHTML1(data){
+        const html = `<p>It's allergy season, how many coughdrops did I eat today?</p>`;
+        return html;
     }
-    getData(); 
-
-
-
-
-
-
-
-
-/* 
-    function updateH1() {
-        console.log(currentTime);
-        const currentCoughDrops = globalData[currentTime];
-        h1Element.innerHTML = `At ${currentTime} I ate ${currentCoughDrops} cough drops.`;
+    
+    function createSelectList(data){
+        let html = '<option>---</option>';
+        const dataPoints = Object.keys(data);
+        //console.log(dataPoints);
+        dataPoints.forEach( function(eachPoint){
+            html += `<option value="${eachPoint}">${data[eachPoint].time}</option>`;
+        } );
+        return html;
     }
+    
+    document.querySelector("#picker").addEventListener('change', function(){
+        const newValue = this.value;
+        updateInterface(newValue, globalData);
 
-    rightIcon.addEventListener('click',function(){
-        const times = Object.keys(globalData);
-        const currentIndex = times.indexOf(currentTime);
-        if (currentIndex > 0) {
-            currentTime = times[currentIndex - 1];
-            updateH1();
-        }
-    });
-
-    leftIcon.addEventListener('click',function(){
-        const times = Object.keys(globalData);
-        const currentIndex = times.indexOf(currentTime);
-        if (currentIndex < times.length - 1) {
-            currentTime = times[currentIndex + 1];
-            updateH1();
-        }
     });
     
+    function updateInterface(value, jsonData ){
+        let html = '<p>';
+        html += `At ${jsonData[value].time} I ate ${jsonData[value].amount} cough  drops.`;
+        html += '</p>';
+
+        document.querySelector("#result").innerHTML = html;
+
+        if (jsonData[value].amount == 4) {
+            document.querySelector("#imageupdate").src = "images/drops4.png";
+        } else if (jsonData[value].amount == 3){
+            document.querySelector("#imageupdate").src = "images/drops3.png";
+        } else if (jsonData[value].amount == 2){
+            document.querySelector("#imageupdate").src = "images/drops2.png";
+        } else if (jsonData[value].amount == 1){
+            document.querySelector("#imageupdate").src = "images/drops1.png";
+        } else {
+            // Optionally, reset the image to a default if none of the conditions match
+            document.querySelector("#imageupdate").src = "../images/cd.png";
+        }
+    }
     
-    getData(); */
+    getData();
     
 })();
