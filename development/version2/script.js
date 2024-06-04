5b21b79a5e66b23f5a2af6041d8cddcc6211376e
myuma24 <147002196+myuma24@users.noreply.github.com>
2024-06-04 00:29:34 -0700

    v2

---
+++ development/version2/script.js
@@ -0,0 +1,46 @@
+(function(){
+    'use strict'
+    let money = 1920;
+    let health = 100;
+    let grades = 100;
+    let isReminderSet = false;
+
+    function updateMoney(){
+        document.getElementById('MoneyCount').innerHTML = `Money: $${money}`;
+    }
+
+    document.getElementById('scene1op1').addEventListener('click', function() {
+        money -= 90;
+        document.getElementById('scene2').style.display = 'block';
+        document.getElementById('scene1').style.display = 'none';
+        updateMoney();
+    });
+
+    document.getElementById('scene1op2').addEventListener('click', function() {
+        let isReminderSet = true;
+        document.getElementById('scene2').style.display = 'block';
+        document.getElementById('scene1').style.display = 'none';
+        updateMoney();    
+    });
+
+
+    function countGrades(){
+        if (grades >= 90){
+
+        } else if (grades >= 80) {
+
+        } else if (grades >= 70) {
+
+        } else if (grades >= 60 ) {
+
+        } else {
+            gradeCounter.innerHtml = "you failed your classes"
+        }
+    }
+
+})();
+
+
+
+
+
